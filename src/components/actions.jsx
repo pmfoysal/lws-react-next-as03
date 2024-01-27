4a8177f7d28a33b91fd383dcbf7e18ae77aaf06e
pmfoysal <pmfoysal@gmail.com>
2024-01-27 17:29:27 +0600

    AddTask Modal Component Updated

--- src/components/actions.jsx
+++ src/components/actions.jsx
@@ -1,12 +1,17 @@
 import Button from './button';
 import Search from './search';
+import { useState } from 'react';
+import TaskModal from './taskModal';
 
 export default function Actions() {
+	const [isOpen, setIsOpen] = useState(true);
+
 	return (
 		<div className='flex items-center space-x-5'>
 			<Search />
-			<Button color='info' name='Add Task' />
+			<Button color='info' name='Add Task' onClick={() => setIsOpen(true)} />
 			<Button color='danger' name='Delete All' />
+			{isOpen ? <TaskModal mode='add' onClose={() => setIsOpen(false)} /> : null}
 		</div>
 	);
 }
