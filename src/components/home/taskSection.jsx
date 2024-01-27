5f27e4c01242fe78cefe1627b369a983684c82b1
pmfoysal <pmfoysal@gmail.com>
2024-01-27 22:26:51 +0600

    No Task Content Added

--- src/components/home/taskSection.jsx
+++ src/components/home/taskSection.jsx
@@ -16,33 +16,39 @@ export default function TaskSection() {
 						<Actions />
 					</div>
 					<div className='overflow-auto'>
-						<Table>
-							{tasks.map(item => (
-								<Row key={item.id}>
-									<Cell>
-										<Icons name={!item.isFavourite ? 'star' : 'star-fill'} />
-									</Cell>
-									<Cell>{item.title}</Cell>
-									<Cell>{item.description}</Cell>
-									<Cell>
-										<ul className='flex justify-center gap-1.5 flex-wrap'>
-											{item.tags.map((tv, ti) => (
-												<li key={tv}>
-													<Tag index={ti} name={tv} />
-												</li>
-											))}
-										</ul>
-									</Cell>
-									<Cell className='text-center capitalize'>{item.priority}</Cell>
-									<Cell>
-										<div className='flex items-center justify-center space-x-3'>
-											<button className='text-red-500'>Delete</button>
-											<button className='text-blue-500'>Edit</button>
-										</div>
-									</Cell>
-								</Row>
-							))}
-						</Table>
+						{tasks.length ? (
+							<Table>
+								{tasks.map(item => (
+									<Row key={item.id}>
+										<Cell>
+											<Icons name={!item.isFavourite ? 'star' : 'star-fill'} />
+										</Cell>
+										<Cell>{item.title}</Cell>
+										<Cell>{item.description}</Cell>
+										<Cell>
+											<ul className='flex justify-center gap-1.5 flex-wrap'>
+												{item.tags.map((tv, ti) => (
+													<li key={tv}>
+														<Tag index={ti} name={tv} />
+													</li>
+												))}
+											</ul>
+										</Cell>
+										<Cell className='text-center capitalize'>{item.priority}</Cell>
+										<Cell>
+											<div className='flex items-center justify-center space-x-3'>
+												<button className='text-red-500'>Delete</button>
+												<button className='text-blue-500'>Edit</button>
+											</div>
+										</Cell>
+									</Row>
+								))}
+							</Table>
+						) : (
+							<h1 className='text-xl text-[#F5BF42] text-center py-12 bg-[#191D26] rounded-xl'>
+								No Tasks Found, Please add one!
+							</h1>
+						)}
 					</div>
 				</div>
 			</div>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,16 +1,26 @@
 import Icons from './icons';
 
-export default function Search() {
+export default function Search({ value, onSearch }) {
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	function handleChange(event) {
+		onSearch(event.target.value);
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='flex'>
 				<div className='relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]'>
 					<input
-						type='search'
-						id='search-dropdown'
-						className='z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none'
-						placeholder='Search Task'
 						required
+						type='search'
+						value={value}
+						id='search-dropdown'
+						onChange={handleChange}
+						placeholder='Search Task'
+						className='z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none'
 					/>
 					<button type='submit' className='absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4'>
 						<Icons name='search' />
