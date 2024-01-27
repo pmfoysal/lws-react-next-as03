84f97114bcda6eaebc7d2fdedd9533cdf0229d7a
pmfoysal <pmfoysal@gmail.com>
2024-01-27 16:13:21 +0600

    Tag Colors Data Added

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -1,7 +1,9 @@
-export default function Tag({ name, color }) {
+import colors from '@/data/colors.json';
+
+export default function Tag({ name, index }) {
 	return (
 		<span
-			className={`inline-block h-5 whitespace-nowrap rounded-[45px] bg-[${colors[color].back}] px-2.5 text-sm capitalize text-[${colors[color].front}]`}>
+			className={`inline-block h-5 whitespace-nowrap rounded-[45px] bg-[${colors[index].back}] px-2.5 text-sm capitalize text-[${colors[index].front}]`}>
 			{name}
 		</span>
 	);
